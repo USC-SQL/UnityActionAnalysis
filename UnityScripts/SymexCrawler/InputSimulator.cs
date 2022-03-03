e22fe4ea0403ca249cb04a243f4ef01b2b47bd41
Sasha Volokh <volokh@usc.edu>
2022-03-03 15:45:22 -0800

    initial implementation of random input crawler done

--- UnityScripts/SymexCrawler/InputSimulator.cs
+++ UnityScripts/SymexCrawler/InputSimulator.cs
@@ -11,49 +11,131 @@ namespace UnitySymexCrawler
 
         private VirtualKeyCode ConvertUnityKeyCode(KeyCode keyCode)
         {
-            if (keyCode == KeyCode.LeftArrow)
+            switch (keyCode)
             {
-                return VirtualKeyCode.LEFT;
-            }
-            else if (keyCode == KeyCode.RightArrow)
-            {
-                return VirtualKeyCode.RIGHT;
-            }
-            else if (keyCode == KeyCode.UpArrow)
-            {
-                return VirtualKeyCode.UP;
-            }
-            else if (keyCode == KeyCode.DownArrow)
-            {
-                return VirtualKeyCode.DOWN;
-            }
-            else if (keyCode == KeyCode.Escape)
-            {
-                return VirtualKeyCode.ESCAPE;
-            }
-            else if (keyCode == KeyCode.Space)
-            {
-                return VirtualKeyCode.SPACE;
-            }
-            else if (keyCode == KeyCode.LeftControl)
-            {
-                return VirtualKeyCode.LCONTROL;
-            }
-            else if (keyCode == KeyCode.LeftShift)
-            {
-                return VirtualKeyCode.LSHIFT;
-            }
-            else if (keyCode == KeyCode.LeftAlt)
-            {
-                return VirtualKeyCode.MENU;
-            }
-            else if (keyCode >= KeyCode.A && keyCode <= KeyCode.Z)
-            {
-                return (VirtualKeyCode)((int)keyCode - 32);
-            }
-            else
-            {
-                return 0;
+                case KeyCode.Backspace:
+                    return VirtualKeyCode.BACK;
+                case KeyCode.Return:
+                    return VirtualKeyCode.RETURN;
+                case KeyCode.Pause:
+                    return VirtualKeyCode.PAUSE;
+                case KeyCode.LeftArrow:
+                    return VirtualKeyCode.LEFT;
+                case KeyCode.RightArrow:
+                    return VirtualKeyCode.RIGHT;
+                case KeyCode.UpArrow:
+                    return VirtualKeyCode.UP;
+                case KeyCode.DownArrow:
+                    return VirtualKeyCode.DOWN;
+                case KeyCode.Escape:
+                    return VirtualKeyCode.ESCAPE;
+                case KeyCode.Space:
+                    return VirtualKeyCode.SPACE;
+                case KeyCode.LeftControl:
+                    return VirtualKeyCode.LCONTROL;
+                case KeyCode.LeftShift:
+                    return VirtualKeyCode.LSHIFT;
+                case KeyCode.LeftAlt:
+                    return VirtualKeyCode.MENU;
+                case KeyCode.RightControl:
+                    return VirtualKeyCode.RCONTROL;
+                case KeyCode.RightShift:
+                    return VirtualKeyCode.RSHIFT;
+                case KeyCode.Comma:
+                    return VirtualKeyCode.OEM_COMMA;
+                case KeyCode.Minus:
+                    return VirtualKeyCode.OEM_MINUS;
+                case KeyCode.Period:
+                    return VirtualKeyCode.OEM_PERIOD;
+                case KeyCode.Alpha0:
+                    return VirtualKeyCode.VK_0;
+                case KeyCode.Alpha1:
+                    return VirtualKeyCode.VK_1;
+                case KeyCode.Alpha2:
+                    return VirtualKeyCode.VK_2;
+                case KeyCode.Alpha3:
+                    return VirtualKeyCode.VK_3;
+                case KeyCode.Alpha4:
+                    return VirtualKeyCode.VK_4;
+                case KeyCode.Alpha5:
+                    return VirtualKeyCode.VK_5;
+                case KeyCode.Alpha6:
+                    return VirtualKeyCode.VK_6;
+                case KeyCode.Alpha7:
+                    return VirtualKeyCode.VK_7;
+                case KeyCode.Alpha8:
+                    return VirtualKeyCode.VK_8;
+                case KeyCode.Alpha9:
+                    return VirtualKeyCode.VK_9;
+                case KeyCode.Delete:
+                    return VirtualKeyCode.DELETE;
+                case KeyCode.Keypad0:
+                    return VirtualKeyCode.NUMPAD0;
+                case KeyCode.Keypad1:
+                    return VirtualKeyCode.NUMPAD1;
+                case KeyCode.Keypad2:
+                    return VirtualKeyCode.NUMPAD2;
+                case KeyCode.Keypad3:
+                    return VirtualKeyCode.NUMPAD3;
+                case KeyCode.Keypad4:
+                    return VirtualKeyCode.NUMPAD4;
+                case KeyCode.Keypad5:
+                    return VirtualKeyCode.NUMPAD5;
+                case KeyCode.Keypad6:
+                    return VirtualKeyCode.NUMPAD6;
+                case KeyCode.Keypad7:
+                    return VirtualKeyCode.NUMPAD7;
+                case KeyCode.Keypad8:
+                    return VirtualKeyCode.NUMPAD8;
+                case KeyCode.Keypad9:
+                    return VirtualKeyCode.NUMPAD9;
+                case KeyCode.Insert:
+                    return VirtualKeyCode.INSERT;
+                case KeyCode.Home:
+                    return VirtualKeyCode.HOME;
+                case KeyCode.End:
+                    return VirtualKeyCode.END;
+                case KeyCode.F1:
+                    return VirtualKeyCode.F1;
+                case KeyCode.F2:
+                    return VirtualKeyCode.F2;
+                case KeyCode.F3:
+                    return VirtualKeyCode.F3;
+                case KeyCode.F4:
+                    return VirtualKeyCode.F4;
+                case KeyCode.F5:
+                    return VirtualKeyCode.F5;
+                case KeyCode.F6:
+                    return VirtualKeyCode.F6;
+                case KeyCode.F7:
+                    return VirtualKeyCode.F7;
+                case KeyCode.F8:
+                    return VirtualKeyCode.F8;
+                case KeyCode.F9:
+                    return VirtualKeyCode.F9;
+                case KeyCode.F10:
+                    return VirtualKeyCode.F10;
+                case KeyCode.F11:
+                    return VirtualKeyCode.F11;
+                case KeyCode.F12:
+                    return VirtualKeyCode.F12;
+                case KeyCode.F13:
+                    return VirtualKeyCode.F13;
+                case KeyCode.F14:
+                    return VirtualKeyCode.F14;
+                case KeyCode.F15:
+                    return VirtualKeyCode.F15;
+                case KeyCode.Help:
+                    return VirtualKeyCode.HELP;
+                default:
+                    if (keyCode >= KeyCode.A && keyCode <= KeyCode.Z)
+                    {
+                        return (VirtualKeyCode)((int)keyCode - 32);
+                    }
+                    else
+                    {
+                        return 0;
+                    }
             }
         }
 
---
+++ UnityScripts/SymexCrawler/RandomInputCrawler.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnitySymexCrawler
+{
+    public class RandomInputCrawler : MonoBehaviour
+    {
+        public float Interval = 0.1f;
+
+        public List<string> ExcludeKeys = new List<string>() {
+            KeyCode.None.ToString(),
+            KeyCode.Tab.ToString(),
+            KeyCode.LeftWindows.ToString(),
+            KeyCode.RightWindows.ToString(),
+            KeyCode.LeftCommand.ToString(),
+            KeyCode.LeftApple.ToString(),
+            KeyCode.RightCommand.ToString(),
+            KeyCode.RightApple.ToString(),
+            KeyCode.SysReq.ToString(),
+            KeyCode.Print.ToString(),
+            KeyCode.CapsLock.ToString(),
+            KeyCode.Numlock.ToString(),
+            KeyCode.ScrollLock.ToString(),
+            KeyCode.Menu.ToString(),
+            KeyCode.Clear.ToString(),
+            KeyCode.Exclaim.ToString(),
+            KeyCode.DoubleQuote.ToString(),
+            KeyCode.Hash.ToString(),
+            KeyCode.Dollar.ToString(),
+            KeyCode.Percent.ToString(),
+            KeyCode.Ampersand.ToString(),
+            KeyCode.Quote.ToString(),
+            KeyCode.LeftParen.ToString(),
+            KeyCode.RightParen.ToString(),
+            KeyCode.Asterisk.ToString(),
+            KeyCode.Plus.ToString(),
+            KeyCode.Slash.ToString(),
+            KeyCode.Colon.ToString(),
+            KeyCode.Semicolon.ToString(),
+            KeyCode.Less.ToString(),
+            KeyCode.Equals.ToString(),
+            KeyCode.Greater.ToString(),
+            KeyCode.Question.ToString(),
+            KeyCode.At.ToString(),
+            KeyCode.LeftBracket.ToString(),
+            KeyCode.Backslash.ToString(),
+            KeyCode.RightBracket.ToString(),
+            KeyCode.Caret.ToString(),
+            KeyCode.Underscore.ToString(),
+            KeyCode.BackQuote.ToString(),
+            KeyCode.KeypadPeriod.ToString(),
+            KeyCode.KeypadDivide.ToString(),
+            KeyCode.KeypadMultiply.ToString(),
+            KeyCode.KeypadMinus.ToString(),
+            KeyCode.KeypadPlus.ToString(),
+            KeyCode.KeypadEnter.ToString(),
+            KeyCode.KeypadEquals.ToString(),
+            KeyCode.PageUp.ToString(),
+            KeyCode.PageDown.ToString(),
+            KeyCode.RightAlt.ToString(),
+            KeyCode.AltGr.ToString(),
+            KeyCode.Break.ToString(),
+            KeyCode.LeftCurlyBracket.ToString(),
+            KeyCode.Pipe.ToString(),
+            KeyCode.RightCurlyBracket.ToString(),
+            KeyCode.Tilde.ToString()
+        };
+        private List<KeyCode> keyCodes;
+
+        private InputSimulator inputSim;
+
+        private void Start()
+        {
+            inputSim = new InputSimulator();
+
+            keyCodes = new List<KeyCode>();
+            foreach (KeyCode keyCode in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (keyCode >= KeyCode.Mouse0 || ExcludeKeys.Contains(keyCode.ToString()))
+                {
+                    continue;
+                }
+
+                keyCodes.Add(keyCode);
+            }
+
+            StartCoroutine("CrawlLoop");
+        }
+
+        public IEnumerator CrawlLoop()
+        {
+            yield return new WaitForSeconds(Interval);
+            for (; ;)
+            {
+                KeyCode keyCode = keyCodes[UnityEngine.Random.Range(0, keyCodes.Count)];
+                if (Input.GetKey(keyCode))
+                {
+                    inputSim.SimulateKeyUp(keyCode);
+                    yield return new WaitForFixedUpdate();
+                }
+                inputSim.SimulateKeyDown(keyCode);
+                yield return new WaitForSeconds(Interval);
+                inputSim.SimulateKeyUp(keyCode);
+                yield return new WaitForFixedUpdate();
+            }
+        }
+    }
+}

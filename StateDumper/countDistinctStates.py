8508bebd619c4032a590701fa5a641ab55c469d1
Sasha Volokh <volokh@usc.edu>
2022-03-10 16:48:06 -0800

    add time cutoff option to countDistinctStates

--- StateDumper/countDistinctStates.py
+++ StateDumper/countDistinctStates.py
@@ -4,23 +4,28 @@ import hashlib
 import os.path
 import shutil
 
-def countDistinctStates(dumps):
+def countDistinctStates(dumps, maxTime):
     covered = set()
     i = 0
     n = len(dumps)
     for d in dumps:
         filename = os.path.basename(d)
-        with open(d, 'r') as f:
-            s = json.loads(f.read())
-            js = json.dumps(s, sort_keys=True)
-            h = hashlib.sha256(js.encode('utf-8')).hexdigest()
-            covered.add(h)
+        t = float(os.path.splitext(filename)[0].split('-')[1])
+        if t <= maxTime:
+            with open(d, 'r') as f:
+                s = json.loads(f.read())
+                js = json.dumps(s, sort_keys=True)
+                h = hashlib.sha256(js.encode('utf-8')).hexdigest()
+                covered.add(h)
         i += 1
         print('{}/{} processed'.format(i, n))
     return len(covered)
 
 if __name__ == '__main__':
     dumpDir = sys.argv[1]
+    maxTime = float('inf')
+    if len(sys.argv) > 2:
+        maxTime = float(sys.argv[2])
     dumps = [os.path.join(dumpDir, f) for f in os.listdir(dumpDir) if f.endswith('.json')]
-    num = countDistinctStates(dumps)
+    num = countDistinctStates(dumps, maxTime)
     print('{} distinct states'.format(num))

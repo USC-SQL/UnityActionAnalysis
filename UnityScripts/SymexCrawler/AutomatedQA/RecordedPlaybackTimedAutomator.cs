d15747a5db59c43e822c309c9235d29e0f82fc98
Sasha Volokh <volokh@usc.edu>
2022-03-21 21:51:13 -0700

    add Automated QA integration for running a script before crawling

---
+++ UnityScripts/SymexCrawler/AutomatedQA/RecordedPlaybackTimedAutomator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using Unity.AutomatedQA;
+using Unity.RecordedPlayback;
+
+/**
+ * The RecordedPlaybackAutomator does not pass control to the next automator after it finishes playback. 
+ * This automator is a wrapper that specifies when to stop the recording playback after a fixed amount of 
+ * game time has passed.
+ */
+
+public class RecordedPlaybackTimedAutomatorConfig : AutomatorConfig<RecordedPlaybackTimedAutomator>
+{
+    public TextAsset recordingFile = null;
+    public bool loadEntryScene = true;
+    public float stopTime = 5.0f;
+}
+
+public class RecordedPlaybackTimedAutomator : Automator<RecordedPlaybackTimedAutomatorConfig>
+{
+    private RecordedPlaybackAutomator rpa;
+
+    public override void BeginAutomation()
+    {
+        base.BeginAutomation();
+        rpa.BeginAutomation();
+        StartCoroutine(StopAfterWaiting());
+    }
+
+    private IEnumerator StopAfterWaiting()
+    {
+        yield return new WaitForSeconds(config.stopTime);
+        RecordedPlaybackController.Instance.Reset();
+        EndAutomation();
+    }
+
+    public override void Init(RecordedPlaybackTimedAutomatorConfig config)
+    {
+        base.Init(config);
+        rpa = gameObject.AddComponent<RecordedPlaybackAutomator>();
+        RecordedPlaybackAutomatorConfig rpaConfig = new RecordedPlaybackAutomatorConfig();
+        rpaConfig.recordingFile = config.recordingFile;
+        rpaConfig.loadEntryScene = config.loadEntryScene;
+        rpa.Init(rpaConfig);
+    }
+}
---
+++ UnityScripts/SymexCrawler/AutomatedQA/StartCrawlerAutomator.cs
@@ -0,0 +1,17 @@
+﻿using Unity.AutomatedQA;
+using UnityEngine;
+
+public class StartCrawlerAutomatorConfig : AutomatorConfig<StartCrawlerAutomator>
+{
+    public GameObject crawlerPrefab;
+}
+
+public class StartCrawlerAutomator : Automator<StartCrawlerAutomatorConfig>
+{
+    public override void BeginAutomation()
+    {
+        base.BeginAutomation();
+        Instantiate(config.crawlerPrefab);
+        EndAutomation();
+    }
+}
